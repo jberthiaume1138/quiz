ec85f3d2dbc696a312ff4156f9d0250e7aa74a52
Joseph Berthiaume <joenyc23@gmail.com>
2016-04-16 02:29:08 -0400

    more layout and design

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 use strict
 
+document.onload() = restartGame();
+
+document.getElementById('btn-restart').addEventListener('click',function(event) {
+	restartGame();
+});
+
+document.getElementById('btn-next').addEventListener('click',function(event) {
+	// handler to ...
+	// ... submit the answer to the current question
+});	
+
+//need event handler for chosen answer
+
+
+
+
+
 var question1 {
-	index: 1;
+	number	
 	question: 'In the special 3rd Annual issue of this comic book series, these two major characters were married in a huge ceremony attended by much of the rest of the comic book world.',
 	choices: ['Clark Kent and Lois Lane','Sue Storm and Reed Richards','Aquaman and SpongeBob SquarePants','Cyclops and Jean Grey' ],
 	answer: 2;
 	explanation: 'Sue Storm, aka The Invisible Woman, and Reed Richards, aka Mr. Fantastic...'
-}
+};
