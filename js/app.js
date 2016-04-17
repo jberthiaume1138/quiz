19f3d38b0e1039e2bd8b7358e40c0c8c88a36494
Joseph Berthiaume <joenyc23@gmail.com>
2016-04-17 02:44:09 -0400

    javascript

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
-use strict
+'use strict'
 
-document.onload() = restartGame();
+var questionLocation = 0;
+var correctAnswers;
+
+var question = [{
+		number: '1',
+		question: 'In the special 3rd Annual issue of this comic book series, these two major characters were married in a huge ceremony attended by much of the rest of the comic book world.',
+		choices: ['Clark Kent and Lois Lane','Sue Storm and Reed Richards','Aquaman and SpongeBob SquarePants','Cyclops and Jean Grey' ],
+		answer: '2',
+		explanation: 'Sue Storm, aka The Invisible Woman, and Reed Richards, aka Mr. Fantastic...'
+	},
+	{	number: 2,
+		question: 'Who is Fido?',
+		choices: ["A term of endearment Cyclops uses for Wolverine","Batman's giant mechanical tyranasaurus","The code name for Captain America's shield"],
+		answer: 2,
+		explanation: 'blah blah blah'
+	}];
+
+
+document.onload = restartGame();
 
 document.getElementById('btn-restart').addEventListener('click',function(event) {
 	restartGame();
@@ -9,18 +27,31 @@ document.getElementById('btn-restart').addEventListener('click',function(event) 
 document.getElementById('btn-next').addEventListener('click',function(event) {
 	// handler to ...
 	// ... submit the answer to the current question
+	var answer = 1;
+	submitAnswer(answer);
 });	
 
 //need event handler for chosen answer
 
 
+function restartGame(){
+	document.getElementById('question-header').innerHTML = 'Question 1 of 5';
 
+};
 
+function setQuestion() {
+	// moves the game to the current question
+};
 
-var question1 {
-	number	
-	question: 'In the special 3rd Annual issue of this comic book series, these two major characters were married in a huge ceremony attended by much of the rest of the comic book world.',
-	choices: ['Clark Kent and Lois Lane','Sue Storm and Reed Richards','Aquaman and SpongeBob SquarePants','Cyclops and Jean Grey' ],
-	answer: 2;
-	explanation: 'Sue Storm, aka The Invisible Woman, and Reed Richards, aka Mr. Fantastic...'
+function submitAnswer(answer) {
+	// accepts answer choice, evaluates it
+	console.log(questions[questionLocation].answer);
+	if (answer == questions[questionLocation].answer) {
+		correctAnswers++;
+		correct();
+	}
+	else {
+		wrong();
+	}
+
 };
