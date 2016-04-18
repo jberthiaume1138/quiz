645fa1976f150a1791298c9739fb4b5f5197db43
jberthiaume1138 <joenyc23@gmail.com>
2016-04-17 22:32:48 -0400

    moving forward

--- js/app.js
+++ js/app.js
@@ -1,42 +1,8 @@
 'use strict'
 
-var questionLocation = 0;
+var questionIndex = 0;
 var correctAnswers = 0;
 
-var questions = [{
-		number: '1',
-		question: 'In the special 3rd Annual issue of this comic book series, these two characters were married in a huge ceremony attended by much of the of the comic book world.',
-		choices: ['Clark Kent and Lois Lane','Sue Storm and Reed Richards','Aquaman and SpongeBob SquarePants','Cyclops and Jean Grey' ],
-		answer: '2',
-		explanation: 'Sue Storm, aka The Invisible Woman, and Reed Richards, aka Mr. Fantastic...'
-	},
-	{	number: '2',
-		question: 'Who is Fido?',
-		choices: ["A term of endearment Cyclops uses for Wolverine","Batman's giant mechanical tyranasaurus","The code name for Captain America's shield","Punisher's pit bull"],
-		answer: '2',
-		explanation: 'blah blah blah'
-	},
-	{
-		number: '3',
-		question: 'The Man Without Fear is a nickname for:',
-		choices: ['Batman','Ironman','Daredevil','The Flash'],
-		answer: '3',
-		explanation: 'More exposition write later'
-	},
-	{
-		number:'4',
-		question:"Captain America's shield is made of:",
-		choices:['adamantium','vibranium','titanium','sheldonium'],
-		answer: '2',
-		explanation: 'more exposition'
-	},
-	{
-		number: '5',
-		question:'Piotr Nikolaievitch Rasputin is the real name of this member of the X-men.',
-		choices:['Colossus','Magneto','Deadpool','Nightcrawler'],
-		answer: '1',
-		explanation:'blah blah'
-	}];
 
 
 document.onload = restartGame();
@@ -45,48 +11,58 @@ document.getElementById('btn-restart').addEventListener('click',function(event) 
 	restartGame();
 });
 
-document.getElementById('btn-next').addEventListener('click',function(event) {
-	// handler to ... submit the answer to the current question
+function restartGame() {
+	// set question position
+	// 0 out running total variables
+	// load first question
 	
-	// get the answer, then pass it into the evaluator
-	var answer = 2;
-	submitAnswer(answer);
-});	
-
-//need event handler for chosen answer
-
-
-function restartGame(){
-	document.getElementById('question-header').innerHTML = 'Question #1';
+	populateQuestion();
+	console.log(questions[questionIndex].answer);
 };
 
-function submitAnswer(answer) {
-	// accepts answer choice, evaluates it
-	console.log(questions[questionLocation].answer);
-	if (answer == questions[questionLocation].answer) {
-		console.log('correct');
-		console.log(questionLocation);
-		correctAnswers++;
-		var score = '<p id="score"> Score: ' + correctAnswers + ' out of ' + questionLocation + '</p>';
-		document.getElementById('question-header-wrapper').innerHTML += score;
-	}
+document.getElementById('btn-next').addEventListener('click',function(event) {
+	// handler to ... submit the answer to the current question
+	// console.log('you selected ' + document.querySelector('input[name=choices]:checked').value);
+	submitAnswer(document.querySelector('input[name=choices]:checked').value);
+});	
 
+function populateQuestion () {
+	document.getElementById('1').innerHTML = questions[questionIndex].choices['0'];
+}
+
+function submitAnswer(choice) {		// evaluate the choice agsinst the quiz answer
+	if (choice == questions[questionIndex].answer) {
+		console.log('Question ' + questionIndex + ' correct!');
+		console.log(questions[questionIndex].choices[choice]);
+		correctAnswers++; // increment the total number of correct answers
+
+		var score = '<p id="score"> Score: ' + correctAnswers + ' out of ' + questionIndex + '</p>';
+		document.getElementById('question-header-wrapper').innerHTML += score;
+
+		correct();
+	}
 	else {
-		console.log('wrong');
-		// wrong();
+		wrongAnswer();
 	}
 
 };
 
 function correct() {
-	document.getElementById('question-header').innerHTML = 'Question' ;
+	// change a bunch of output to show correct choice
+	var output = '<h2>Correct!</h2><p>' + questions[questionIndex].explanation + '</p>';
+
+	document.getElementById('explanation').innerHTML += output;
+	//document.getElementById('explanation');
 
 };
 
-function explanation() {
-
+function wrongAnswer () {
+	// generate a bunch of output to show wrong choice
+	console.log('wrong!');
 };
 
-function endGame() {
-
+function gameOver() {
+	// logic branch to generate output based on final score
+	// show start over button
+	// clear running total variables, question position etc
 };
---
+++ js/questions.js
@@ -0,0 +1,35 @@
+var questions = [
+	{
+		number: '1',
+		question: 'In the special 3rd Annual issue of this comic book series, these two characters were married in a huge ceremony attended by much of the of the comic book world.',
+		choices: ['Clark Kent and Lois Lane','Sue Storm and Reed Richards','Aquaman and SpongeBob SquarePants','Cyclops and Jean Grey' ],
+		answer: '1',
+		explanation: 'Sue Storm, aka The Invisible Woman, and Reed Richards, aka Mr. Fantastic...'
+	},
+	{	number: '2',
+		question: 'Who is Fido?',
+		choices: ["A term of endearment Cyclops uses for Wolverine","Batman's giant mechanical tyranasaurus","The code name for Captain America's shield","Punisher's pit bull"],
+		answer: '1',
+		explanation: 'blah blah blah'
+	},
+	{
+		number: '3',
+		question: 'The Man Without Fear is a nickname for:',
+		choices: ['Batman','Ironman','Daredevil','The Flash'],
+		answer: '2',
+		explanation: 'More exposition write later'
+	},
+	{
+		number:'4',
+		question:"Captain America's shield is primarily made of:",
+		choices:['adamantium','vibranium','titanium','sheldonium'],
+		answer: '1',
+		explanation: 'more exposition'
+	},
+	{
+		number: '5',
+		question:'Piotr Nikolaievitch Rasputin is the real name of this member of the X-men.',
+		choices:['Colossus','Magneto','Deadpool','Nightcrawler'],
+		answer: '0',
+		explanation:'blah blah'
+	}];
