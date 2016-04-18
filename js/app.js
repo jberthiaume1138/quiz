61771bd3a652ff7ce77aba83a37996feef0de0cb
jberthiaume1138 <joenyc23@gmail.com>
2016-04-18 03:01:12 -0400

    Basic functionality complete

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 'use strict'
 
+// TODO:
+// question explanations
+// running total of score + positioning
+// all the CSS
+
 var questionIndex;
 var correctAnswers;
 
@@ -10,7 +15,7 @@ document.getElementById('btn-restart').addEventListener('click',function(event) 
 });
 
 function restartGame() {
-	// resets running totales, moves to first question, populates fields
+	// resets running totals, moves to first question, populates fields
 	questionIndex = 0;
 	correctAnswers = 0;
 
@@ -19,27 +24,25 @@ function restartGame() {
 	document.getElementById('game-over').style.display = 'none';
 
 	populateQuestion();
-//	console.log(questions[questionIndex].answer);
 };
 
 document.getElementById('btn-next').addEventListener('click',function(event) {
 	// handler to get the selected choice and submit it for evaluation
-	submitAnswer(document.querySelector('input[name=choices]:checked').value);
+	evaluateAnswer(document.querySelector('input[name=choices]:checked').value);
 });	
 
 function populateQuestion () {
-	// first load the question from the file
+	// first load the question from the array of objects in the file
 	document.getElementById('question').innerHTML = questions[questionIndex].question;
-	//then load the choices from the file
+	//then load the choices from the array of objects in the file for that question
 	var listOfLabels = document.getElementsByClassName('choice-labels');
 	for (var i = 0; i < listOfLabels.length; i++) {
 		listOfLabels.item(i).innerHTML = questions[questionIndex].choices[i];
-		// console.log (questions[questionIndex].choices[i]);
-		// console.log (listOfLabels.item(i).innerHTML);
 	}
+	document.getElementById('question-header').innerHTML = 'Question #' + (questionIndex + 1);
 }
 
-function submitAnswer(choice) {		// evaluate the choice agsinst the quiz answer
+function evaluateAnswer(choice) {		// evaluate the choice agsinst the quiz answer
 	if (choice == questions[questionIndex].answer) {
 		console.log('Question ' + questionIndex + ' correct!');
 		console.log(questions[questionIndex].choices[choice]);
@@ -92,6 +95,7 @@ function gameOver() {
 
 	if (correctAnswers == questions.length) {
 		gameOverSplash.innerHTML += '<h2>Excelsior!</h2><br/><h3>You are a champion! Worthy of joining the X-men, the Avengers or the Justice League.<br/>Just remember, with great power, comes great responsibility.';
+		gameOverSplash.innerHTML += '<img src="images/stanlee.jpg">';
 	}
 	else if ((correctAnswers >= 3) && (correctAnswers <= 5)) {
 		gameOverSplash.innerHTML += '<h2>Well done shell-head!</h2><br/><h3>You are clearly an expert worthy of a spot in Charles Xavier\'s School.</h3>';
@@ -100,10 +104,9 @@ function gameOver() {
 		gameOverSplash.innerHTML += '<h2>Not ready for the Justice League just yet.</h2><br/><h3>You should head over to the Danger Room for more training. Watch out for for Wolverine\'s claws.</h3>'; 
 	}
 	else if (correctAnswers <= 1) {
-		gameOverSplash.innerHTML += '<h2>Sad face.</h2><h3>Stop wasting time taking internet quizes and go to the comic book store before the Hulk sits on your head.</h3>';
+		gameOverSplash.innerHTML += '<h2>Sad face</h2><h3>Stop wasting time taking internet quizes and go to the comic book store before the Hulk sits on your head.</h3>';
+		gameOverSplash.innerHTML += '<img src="images/hulk.gif">';
 	}
-
-	gameOverSplash.innerHTML += '<img src="images/stanlee.jpg">';
 
 	gameOverSplash.innerHTML += '<button id="play-again">Play Again</button>';
 
