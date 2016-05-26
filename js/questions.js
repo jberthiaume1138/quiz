12cc7ed861e2e78a86a8cb65c4fab63095d5babc
jberthiaume1138 <joenyc23@gmail.com>
2016-05-25 23:37:55 -0400

    question #1 copy edit

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,7 @@
 var questions = [
 	{
 		number: '1',
-		question: 'In the special 3rd Annual issue of this comic book series, these two characters were married in a huge ceremony attended by much of the of the comic book world.',
+		question: 'In the special 3rd Annual issue of this comic book series, these two characters were married in a huge ceremony attended by much of the comic book world.',
 		choices: ['Clark Kent and Lois Lane','Sue Storm and Reed Richards','Aquaman and SpongeBob SquarePants','Cyclops and Jean Grey' ],
 		answer: '1',
 		explanation: 'Sue Storm, aka The Invisible Woman, and Reed Richards, aka Mr. Fantastic...'
